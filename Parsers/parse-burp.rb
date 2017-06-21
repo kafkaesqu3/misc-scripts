801195c5670950605de8cbcdd0bc6e86e39bb53b
root <root@localhost.localdomain>
2017-06-21 15:52:55 -0400

    Added burp parser

---
+++ Parsers/parse-burp.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+# Copyright (C) 2013 www.pentestgeek.com Royce Davis (@r3dy__)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+# #
+
+# Got this from https://github.com/R3dy/parse-burp
+begin
+  require 'nokogiri'
+  require 'csv'
+  require 'optparse'
+rescue LoadError
+    puts 'Missing gems. Make sure to install: csv, nokogiri using \'gem install <gem>\''
+    exit
+end
+
+# This is the main parsing method which will run on each 
+# finding in 'issue' (or finding) in the burp XML file.
+def clean_finding(finding)
+  output = []
+  output << finding.css('host')[0]["ip"]
+  output << finding.css('host').text
+  output << finding.css('path').text
+  output << finding.css('severity').text
+  output << finding.css('name').text
+  output << finding.css('issueBackground').text
+  output << finding.css('issueDetail').text
+  output << finding.css('remediationBackground').text
+  response = finding.css('response').text
+  if response.include?('Server:')
+    output << response.split('Server: ')[1].split("\n")[0]
+  end
+  output
+end
+
+options = {}
+
+optparse = OptionParser.new do|opts|
+    opts.banner = "Parse Burp Suite XML output into CSV results.\r\nUsage: #{$0} [options]"
+
+    opts.on('-i', '--infile FILE', 'Input XML file') do |file|
+      raise 'No such file' unless File.exists?(file)
+      options[:infile] = file
+    end
+
+    opts.on('-o', '--outfile FILE', 'Output CSV file') do |file|
+      options[:outfile] = file
+    end
+
+    options[:help] = opts.help
+end
+
+# Parse the arguments to the script
+optparse.parse!
+begin
+  raise OptionParser::MissingArgument if options[:infile].nil?
+  raise OptionParser::MissingArgument if options[:outfile].nil?
+rescue OptionParser::MissingArgument
+  puts options[:help]
+  exit
+end
+
+# Create an XML object from the file provided at runtime
+report = Nokogiri::XML(File.open(options[:infile]))
+
+# This is just a string that serves as the title line of the CSV output
+CSV.open(options[:outfile], 'w') do |csv|
+  csv << [
+    'IP', 
+	'Host',
+	'Path',
+    'Severity',
+	'Name',
+    'Issue Background',
+    'Issue Detail',
+    'Recommendation',
+]
+    report.xpath('//issues/issue').each do |finding|
+        csv << clean_finding(finding)
+    end
+end
+
+
