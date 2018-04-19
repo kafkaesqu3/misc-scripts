46c1a97dd54d8cc9df1b6a199e10c63f21f8e052
root <root@localhost.localdomain>
2018-04-19 00:41:58 -0400

    bug fixes in 12 line python script

--- Parsers/parseports.py
+++ Parsers/parseports.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
+
+# takes CSV export results of ports table from recon-ng, and parses the results into files containing IP addresses grouped by port
+
 import re
 
 f = open('results.csv')
 
 for line in f:
     p = line.split(',')
-    f2 = open(str(re.sub('[\'\"]', '', p[2])), "w+")
-    f2.write(p[0])
+    f2 = open(str(re.sub('[\'\"]', '', p[2])), "a+")
+    f2.write(str('{0}\n'.format(re.sub('[\'\"]', '', p[0]))))
     f2.close()
 
 f.close()
