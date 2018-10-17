74ce6a8335742fe136cf41e5171f58257c9354c3
david <kafkaesque0@protonmail.com>
2018-10-17 00:08:19 -0400

    GOGOBUSTER: concurrent gobuster

--- Fuzzers/gogobuster.py
+++ Fuzzers/gogobuster.py
@@ -1,51 +1,65 @@
-# System modules
+#######################################################################
+#######################################################################
+###########                                                 ###########
+########### GOGOBUSTER: Parallel directory bruteforcing     ###########
+########### Scan multiple URLs concurrently with gobuster   ###########
+###########                                                 ###########
+#######################################################################
+#######################################################################
+
+# Gobuster was written by OJ Reeves @TheColonial
+# https://github.com/OJ/gobuster
+
+# Already have scanned a network with EyeWitness? https://github.com/FortyNorthSecurity/EyeWitness
+# MiktoList.py can take the scan output and create a list of URLs with splash pages or 404
+# https://github.com/FortyNorthSecurity/EyeWitness/blob/master/MiktoList.py
+
 from Queue import Queue
 from threading import Thread
-import time
+import argparse
+import subprocess
+import re
 
-# Local modules
-import feedparser
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-f", "--file", help="File with target URLs", required=True)
+argparser.add_argument("-w", "--wordlist", help="Wordlist to use across all URLs", required=True)
+argparser.add_argument("-o", "--output", help="Output directory", required=False)
+argparser.add_argument("-t", "--threads", type=int, help="# of threads (concurrent URLs to be scanned)", required=False)
+arguments = argparser.parse_args()
 
-# Set up some global variables
-thread_count = 2
-command_queue = Queue()
-
-file = "test"
+file = arguments.file
 targets = open(file).read().splitlines()
 
+wordlist = arguments.wordlist
+output_folder = arguments.output
+
+if arguments.threads is None: 
+    thread_count = 5
+else: 
+    thread_count = arguments.threads
+
+command_queue = Queue()
 
 def runCommand(item, queue):
     while True:
-        print '%s: Looking for the next enclosure' % item
         command = queue.get()
-        print '%s: Downloading:' % item, url
-        # instead of really downloading the URL,
-        # we just pretend and sleep
-        time.sleep(item + 2)
+        #print(command.split(' '))
+        subprocess.call(command.split(' '))
         queue.task_done()
 
-
-# Set up some threads to fetch the enclosures
+# Set up some threads to run our gobuster commands
 for item in range(thread_count):
-    worker = Thread(target=runCommand, args=(iitem, command,))
+    worker = Thread(target=runCommand, args=(item, command_queue))
     worker.setDaemon(True)
     worker.start()
 
-# Download the feed(s) and put the enclosure URLs into
-# the queue.
+# Build each command string and add it to the queue
 for url in targets:
-    
-
-
-    
-    response = feedparser.parse(url, agent='fetch_podcasts.py')
-    for entry in response['entries']:
-        for enclosure in entry.get('enclosures', []):
-            print 'Queuing:', enclosure['url']
-            enclosure_queue.put(enclosure['url'])
+    output_file = re.sub(r"https?://", '', url)
+    command = "gobuster -m dir -e -l -k -u {0} -w {1} -o {2}/{3}_out".format(url, wordlist, output_folder, output_file)
+    command_queue.put(command)
         
-# Now wait for the queue to be empty, indicating that we have
-# processed all of the downloads.
-print '*** Main thread waiting'
-enclosure_queue.join()
-print '*** Done'
+# when the queue is empty, we're done
+print('Thread waiting...')
+command_queue.join()
+print('Done!)
