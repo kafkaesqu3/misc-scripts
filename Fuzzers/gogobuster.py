46e7b88040fc1797ac850351fe599f3b77517d88
david <david.tulis@nccgroup.com>
2018-10-17 11:16:43 -0400

    refactored to use thread pool instead of thread queue. now we can ctrl-c to cancel scan

--- Fuzzers/gogobuster.py
+++ Fuzzers/gogobuster.py
@@ -14,52 +14,55 @@
 # MiktoList.py can take the scan output and create a list of URLs with splash pages or 404
 # https://github.com/FortyNorthSecurity/EyeWitness/blob/master/MiktoList.py
 
-from Queue import Queue
-from threading import Thread
+import multiprocessing
 import argparse
 import subprocess
 import re
+import sys
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument("-f", "--file", help="File with target URLs", required=True)
-argparser.add_argument("-w", "--wordlist", help="Wordlist to use across all URLs", required=True)
-argparser.add_argument("-o", "--output", help="Output directory", required=False)
-argparser.add_argument("-t", "--threads", type=int, help="# of threads (concurrent URLs to be scanned)", required=False)
-arguments = argparser.parse_args()
+def runCommand(command):
+    subprocess.call(command.split(' '))
 
-file = arguments.file
-targets = open(file).read().splitlines()
 
-wordlist = arguments.wordlist
-output_folder = arguments.output
+def pool_function(command):
+    try:
+        return runCommand(command)
+    except KeyboardInterrupt:
+        return 'KeyboardException'
 
-if arguments.threads is None: 
-    thread_count = 2
-else: 
-    thread_count = arguments.threads
+def main(): 
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-f", "--file", help="File with target URLs", required=True)
+    argparser.add_argument("-w", "--wordlist", help="Wordlist to use across all URLs", required=True)
+    argparser.add_argument("-o", "--output", help="Output directory", required=False)
+    argparser.add_argument("-t", "--threads", type=int, help="# of threads (concurrent URLs to be scanned)", required=False)
+    arguments = argparser.parse_args()
 
-command_queue = Queue()
+    file = arguments.file
+    targets = open(file).read().splitlines()
 
-def runCommand(item, queue):
-    while True:
-        command = queue.get()
-        #print(command.split(' '))
-        subprocess.call(command.split(' '))
-        queue.task_done()
+    wordlist = arguments.wordlist
+    output_folder = arguments.output
 
-# Set up some threads to run our gobuster commands
-for item in range(thread_count):
-    worker = Thread(target=runCommand, args=(item, command_queue))
-    worker.setDaemon(True)
-    worker.start()
+    if arguments.threads is None: 
+        thread_count = 2
+    else: 
+        thread_count = arguments.threads
 
-# Build each command string and add it to the queue
-for url in targets:
-    output_file = re.sub(r"https?://", '', url)
-    command = "gobuster -m dir -e -l -k -u {0} -w {1} -o {2}/{3}_out".format(url, wordlist, output_folder, output_file)
-    command_queue.put(command)
-        
-# when the queue is empty, we're done
-print('Thread waiting...')
-command_queue.join()
-print('Done!')
+    pool = multiprocessing.Pool(thread_count)
+
+    try:
+        jobs = []
+        for url in targets: 
+            output_file = re.sub(r"https?://", '', url)
+            command = "gobuster -m dir -e -l -k -u {0} -w {1} -o {2}/{3}_out".format(url, wordlist, output_folder, output_file)
+
+            jobs.append(pool.apply_async(pool_function, args=([command])))
+        pool.close()
+        pool.join()
+    except KeyboardInterrupt:
+        print 'parent received control-c'
+        pool.terminate()
+
+if __name__ == "__main__":
+    main()
