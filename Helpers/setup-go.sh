63da0e1a931d0a0dcf57853f83140a507a3d7f26
david <david.tulis@nccgroup.com>
2018-11-26 10:48:47 -0500

    fuck go dude

---
+++ Helpers/setup-go.sh
@@ -0,0 +1,8 @@
+curl -O https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
+tar -xvf go1.11.2.linux-amd64.tar.gz
+mv go /usr/local
+
+cat << EOF >> ~/.bashrc
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+EOF
